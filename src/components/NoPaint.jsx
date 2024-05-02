008ffce9c7f4d258b0ea6414ac8ac28c7ea5325f
abir13719 <abir13719@gmail.com>
2024-05-02 22:07:23 +0600

    Added All Paintings

---
+++ src/components/NoPaint.jsx
@@ -0,0 +1,9 @@
+const NoPaint = () => {
+  return (
+    <div>
+      <h1>You didn&apos;t Added Any Painting</h1>
+    </div>
+  );
+};
+
+export default NoPaint;
--- src/pages/AllPaintings.jsx
+++ src/pages/AllPaintings.jsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const AllPaintings = () => {
+  const [allPainting, setAllPainting] = useState([]);
+  console.log(allPainting);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/painting")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllPainting(data);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>All Paintings here</h1>
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+      {allPainting.map((paint) => (
+        <div
+          key={paint._id}
+          className="my-5 bg-gradient-to-r from-pink-500 to-orange-500 p-5 rounded-xl"
+          data-aos="slide-up"
+        >
+          <img src={paint.photoURL} data-aos="zoom-in" />
+          <h1 className="font-bold text-xl my-2">{paint.paintName}</h1>
+
+          <div className="flex justify-between">
+            <p className="font-medium">Price: ${paint.price}</p>
+            <p>Rating: {paint.rating}</p>
+          </div>
+          <div className="flex justify-between">
+            <p>Added by: {paint.userName}</p>
+            <p>Process: {paint.process} minutes</p>
+          </div>
+          <Link to={`/my-painting/${paint._id}`}>
+            <button className="btn w-full bg-black text-white hover:bg-black">View Details</button>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
