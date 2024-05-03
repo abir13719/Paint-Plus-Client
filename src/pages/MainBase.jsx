95d7bc01e1bbbc2a3d53192d457344b5e9faa6c9
abir13719 <abir13719@gmail.com>
2024-05-03 10:47:39 +0600

    Added Footer

--- src/pages/MainBase.jsx
+++ src/pages/MainBase.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 const MainBase = () => {
   return (
     <div className="container mx-auto bg-gradient-to-r from-red-500 via-purple-500 to-pink-500">
       <Nav></Nav>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
